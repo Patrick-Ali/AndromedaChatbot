c39dd69d4b433fd41ad10b7e11860d89499f4793
Patrick-Ali <alip@uni.coventry.ac.uk>
2018-03-09 01:47:24 +0000

    Need to add actions

--- dataControl.py
+++ dataControl.py
@@ -66,41 +66,105 @@ def specificInfoP1(inputText):
 
 def specificInfoP2(inputText, info):
     potentialInfo = []
-    print(inputText)
+    print("Hello 1 ", inputText)
     print(info)
     lastWord = ''
     tempInfo = []
     count = 0
     counting = 0
     keys = []
+    usedKey = []
+    held = []
     for element in info:
         #tempInfo.append(info[counting])
         #counting += 1
         for key in element:
             keys.append(key)
-            print('Key ', key)
-            print('Info ', info[counting][key])
+            print('Key2 ', key)
+            print('Info2 ', info[counting][key])
             tempInfo.append(info[counting][key])
             counting += 1
-    print("Temp info ", tempInfo)    
+    print("Temp info2 ", tempInfo)    
     for element in tempInfo:
-        print("Element ", element)
-        print("Corosponding key ", keys[count])
+        print("Element2 ", element)
+        print("Corosponding key2 ", keys[count])
         for key in element:
             
             for word in inputText:
                 print(word)
                 print(key)
                 print(lastWord+' '+word)
+                tempHold = word.split(" ")
+                if len(tempHold) > 1:
+                    print("tempHold ", tempHold)
+                    counting = 0
+                    for entry in tempHold:
+                        if key == entry or key == (lastWord+' '+entry):
+                            potentialInfo.append({keys[count]:element[key]})
+                            usedKey.append(key + ' ' + tempHold[counting + 1])
+                        lastWord = entry
+                        counting += 1
                 if key == word or key == (lastWord+' '+word):
                     potentialInfo.append({keys[count]:element[key]})
+                    usedKey.append(key)
                 lastWord = word
         count += 1
-    return potentialInfo
+    held.append(potentialInfo)
+    held.append(usedKey)
+    return held #potentialInfo
 
 def compareData(inputText, info):
-    values = specificInfoP2(inputText, info)
+    hold = specificInfoP2(inputText, info)
+    values = hold[0]
     print(values)
+    keyWords = loadData("keyWords")
+    key = ''
+    for i in hold[1]:
+        print("val1 ", i)
+        if i in keyWords["keywords"]:
+            #key += i + ' '
+            key = i
+    print("KeyLime ", key)
+    
+    if key == "Diameter":
+        key = "largest"
+    if key == "Diameter smallest":
+        key = "smallest"
+    print("Yolo Key is ", key)
+    if key == "largest":
+        return compareLargest(values)
+    elif key == "smallest":
+        return compareSmallest(values)
+##    count = 0
+##    rawVals = []
+##    for i in values:
+##        print(i)
+##        for j in i:
+##            print(values[count][j])
+##            print(type(values[count][j]))
+##            rawVals.append(values[count][j])
+##        count += 1
+##    count = 0
+##    highest = rawVals[0]
+##    for val in rawVals:
+##        if val > highest:
+##            highest = val
+##    print(highest)
+##    lp = ''
+##    for i in values:
+##        print(i)
+##        for j in i:
+##            if values[count][j] == highest:
+##                print(j)
+##                lp = j
+##            #print(values[count][j])
+##            #print(type(values[count][j]))
+##            #rawVals.append(values[count][j])
+##        count += 1
+##    
+##    return lp
+
+def getRawValue(values):
     count = 0
     rawVals = []
     for i in values:
@@ -110,39 +174,70 @@ def compareData(inputText, info):
             print(type(values[count][j]))
             rawVals.append(values[count][j])
         count += 1
+    return rawVals
+
+def compareSmallest(values):
+    rawValues = getRawValue(values)
+    smallest = rawValues[0]
+    for val in rawValues:
+        if val < smallest:
+            smallest = val
+    print(smallest)
+    smallestEntry = ''
     count = 0
-    highest = rawVals[0]
-    for val in rawVals:
-        if val > highest:
-            highest = val
-    print(highest)
-    lp = ''
-    for i in values:
-        print(i)
-        for j in i:
-            if values[count][j] == highest:
-                print(j)
-                lp = j
+    for entry in values:
+        print(entry)
+        for key in entry:
+            if values[count][key] == smallest:
+                print(key)
+                smallestEntry = key
             #print(values[count][j])
             #print(type(values[count][j]))
             #rawVals.append(values[count][j])
         count += 1
     
-    return lp
+    return smallestEntry
+
+def compareLargest(values):
+    rawValues = getRawValue(values)
+    largest = rawValues[0]
+    for val in rawValues:
+        if val > largest:
+            largest = val
+    print(largest)
+    largestEntry = ''
+    count = 0
+    for entry in values:
+        print(entry)
+        for key in entry:
+            if values[count][key] == largest:
+                print(key)
+                largestEntry = key
+            #print(values[count][j])
+            #print(type(values[count][j]))
+            #rawVals.append(values[count][j])
+        count += 1
+    
+    return largestEntry
 
 def translate(inputText):
     data = loadData("translate")
     text = inputText
     print(text)
+    #count = 0
     for entry in data:
         print(entry)
-        count = 0
+        #print(count)
+        #count = 0
         for words in data[entry]:
             print(words)
+            count = 0
             for word in text:
                 print(word)
                 if words == word or words.lower() == word:
                     print("Word found ", word)
+                    print(text)
+                    print(count)
                     text[count] = entry
                 count += 1
     print(text)
---
+++ extra.py
@@ -0,0 +1,21 @@
+def compareSmallest(values):
+    rawValues = getRawValue(values)
+    smallest = rawValues[0]
+    for val in rawValues:
+        if val < smallest:
+            largest = val
+    print(largest)
+    smallestEntry = ''
+    count = 0
+    for entry in values:
+        print(entry)
+        for key in entry:
+            if values[count][key] == smallest:
+                print(key)
+                smallestEntry = key
+            #print(values[count][j])
+            #print(type(values[count][j]))
+            #rawVals.append(values[count][j])
+        count += 1
+    
+    return smallestEntry
